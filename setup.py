dcce3628649c14a05490f649ea7942825ba8d8b4
D4rkC4t <samuelcozanet@gmail.com>
2010-12-28 14:54:13 +0100

    Added all variable to __init__.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup, find_packages
 setup(
     name = "deptfr",
-    version = "0.1",
+    version = "0.1.2",
     packages = find_packages(),
-    scripts = ['deptfr.py'],
 )
