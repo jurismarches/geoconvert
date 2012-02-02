b6a09e21598f41f06b95f71f0ac3d97ed9230925
Samuel COZANET <s.cozanet@jurismarches.com>
2012-02-02 15:49:07 +0100

    Release 1.9.2

--- geoconvert/__init__.py
+++ geoconvert/__init__.py
@@ -1,3 +1,3 @@
 __all__ = ['deptfr', 'countrycode']
 __author__ = "Samuel Cozanet <s.cozanet@jurismarches.com>"
-__version__ = "1.9"
+__version__ = "1.9.2"
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
     name = "geoconvert",
-    version = "1.9",
+    version = "1.9.2",
     packages = find_packages(),
 )
