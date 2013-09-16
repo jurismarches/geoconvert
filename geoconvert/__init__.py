1808eb15790c048c64ee1b698cd93624ccae93e7
Geoffrey LEHEE <g.lehee@jurismarches.com>
2013-09-16 14:06:25 +0200

    Updated __author__ and version

--- geoconvert/__init__.py
+++ geoconvert/__init__.py
@@ -1,3 +1,2 @@
-__all__ = ['deptfr', 'countrycode']
-__author__ = "Jurismarches Team <informatique@jurismarches.com>"
-__version__ = "2.0.7"
+__author__ = "Jurismarches <informatique@jurismarches.com>"
+__version__ = "2.1.0"
