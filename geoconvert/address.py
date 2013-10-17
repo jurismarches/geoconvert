5580384b71ae3c2f952a2d7ee76375302f87c41c
Florian PAQUET <f.paquet@jurismarches.com>
2013-10-17 18:32:03 +0200

    Added simple address parser

---
+++ geoconvert/address.py
@@ -0,0 +1,74 @@
+# -*- coding:utf-8 -*-
+import re
+
+
+class Address(object):
+    """
+    French address representation
+    """
+    def __init__(self, zipcode):
+        self.zipcode = zipcode
+
+        if zipcode is not None:
+            self.zipcode_int = int(zipcode)
+
+    def get_department(self):
+        """
+        Returns the department number from the zipcode
+        """
+        if self.zipcode is not None:
+            dept_number = self.zipcode[:2]
+
+            # Dom - Tom
+            if int(dept_number) >= 97:
+                dept_number = self.zipcode[:3]
+
+            # Corse
+            if dept_number == '20':
+                if self.zipcode_int < 20190:
+                    dept_number = '2A'
+                else:
+                    dept_number = '2B'
+
+            return dept_number
+
+
+class AddressParser(object):
+    """
+    Simple french address parser
+    """
+    address_class = Address
+    zipcode_filters_re = re.compile(r'(?P<address>.*?)(?=CEDEX|$)', flags=re.IGNORECASE)
+    zipcode_re = re.compile(
+        r'(?<!BP)(?<!B\.P\.)(?<!CS)(?:[^\d]|^)(?P<zipcode>\d{2}\s*?\d{2,3})',
+        flags=re.IGNORECASE)
+
+    def _parse_zipcode(self, address):
+        """
+        Tries to extract zipcode from address
+        """
+        filter_match = self.zipcode_filters_re.match(address)
+
+        if filter_match:
+            address = filter_match.group('address')
+            match_list = list(self.zipcode_re.finditer(address))
+
+            # It's easier to parse zipcode from the end of the string
+            if match_list:
+                return match_list[-1].group('zipcode').replace(' ', '').zfill(5)
+
+    def get_address_class(self):
+        """
+        Returns address class
+        """
+        return self.address_class
+
+    def parse(self, address):
+        """
+        Parses address and returns an Address object
+        """
+        address_class = self.get_address_class()
+        zipcode = self._parse_zipcode(address)
+
+        return address_class(
+            zipcode=zipcode)
--- test_geoconvert.py
+++ test_geoconvert.py
@@ -8,9 +8,8 @@ import unittest
 from geoconvert.convert import zipcode_to_dept_name
 from geoconvert.convert import address_to_zipcode
 from geoconvert.convert import dept_name_to_zipcode
-from geoconvert.convert import region_id_to_name
-from geoconvert.convert import region_name_to_id
 from geoconvert.convert import country_name_to_id
+from geoconvert.address import AddressParser
 
 
 class GeoconvertTestCase(unittest.TestCase):
@@ -123,6 +122,66 @@ class GeoconvertTestCase(unittest.TestCase):
             ("Country execution:nigeria.", 'NG')]
         for test in data:
             self.assertEqual(country_name_to_id(test[0], lang='EN'), test[1])
+
+
+class AddressParserTestCase(unittest.TestCase):
+    """
+    AddressParser test case
+    """
+    def setUp(self):
+        self.address_parser = AddressParser()
+        self.data = [
+            (u"Chemin du Solarium\\n Le Haut Vigneau\\n 33175 GRADIGNAN CEDEX", '33175', '33'),
+            ("Chemin du Solarium 061256784589 Le Haut Vigneau 33175 GRADIGNAN CEDEX ", '33175', '33'),
+            ("Chemin du Solarium Le Haut Vigneau 33175 GRADIGNAN CEDEX 061256784589", '33175', '33'),
+            ("Chemin du Solarium Le Haut Vigneau 33175 GRADIGNAN CEDEX", '33175', '33'),
+            ('7 cours Grandval\\nBP 414 - 20183 AJACCIO - CEDEX', '20183', '2A'),
+            ('20212   Erbajolo', '20212', '2B'),
+            ('20223   Solenzara Air', '20223', '2B'),
+            ('BP 55342 20223   Solenzara Air', '20223', '2B'),
+            ('Chemin du Solarium Le Haut Vigneau 33 175 GRADIGNAN CEDEX', '33175', '33'),
+            ('20 223   Solenzara Air', '20223', '2B'),
+            ('97821 Le Port Cedex', '97821', '978'),
+            ('27006 Évreux Cedex', '27006', '27'),
+            ('  27006 Évreux Cedex', '27006', '27'),
+            ('27006', '27006', '27'),
+            ('Roissy-en-France95700', '95700', '95'),
+            (' 44200 BP 10720 Nantes cedex', '44200', '44'),
+            ('a l attention de M. Bon Jean, Avenue des clients BP 72152, F - 31020 Toulouse', '31020', '31'),
+            ('a l attention de M. Bon Jean, Avenue des clients BP72152, F - 31020 Toulouse', '31020', '31'),
+            ('a l attention de M. Bon Jean, Avenue des clients bp72152, F - 31020 Toulouse', '31020', '31'),
+            ('Avenue des clients CS 72152, F - 31020 Toulouse', '31020', '31'),
+            ('BP 1330, 6503 TARBES Cedex 9, tel. 05.62.54.58.63', '06503', '06'),
+            ('Ville de Blanquefort, 12 rue Dupaty B.P. 20117, à l attention de fernanda Edant-33294 Blanquefort.', '33294', '33'),
+            ('conseil général du Haut-Rhin, 100 avenue d alsace B.P.20351, conseil général du Haut-Rhin-68006 Colmar Cedex', '68006', '68'),
+            ('Avenue des clients CS 72152, F - 31020 Toulouse', '31020', '31'),
+            ('Avenue des clients CS72152, F - 31020 Toulouse', '31020', '31'),
+            ('6503 TARBES Cedex 9, tel. 05.62.54.58.63', '06503', '06'),
+            ('97700 Saint-Barthelemy', '97700', '977'),
+            ('Mme Cindy DUMAS, 1500 Bd Lepic - B.P. 348 73103 Aix-les-Bains', '73103', '73'),
+            ('Mme Cindy DUMAS, 15000 Bd Lepic - B.P. 348 73103 Aix-les-Bains', '73103', '73'),
+            ('Mme Cindy DUMAS, 15000 Bd Lepic - 73103 B.P. 89348 Aix-les-Bains', '73103', '73'),
+            ('Mme Cindy DUMAS, 15000 Bd Lepic - 73103 b.p. 89348 Aix-les-Bains', '73103', '73'),
+            ('Mme Cindy DUMAS, 15000 Bd Lepic - 73103 bp 89348 Aix-les-Bains', '73103', '73'),
+            ('a l attention de M. Bon Jean, Avenue des client', None, None)
+        ]
+
+    def test_zipcode(self):
+        """
+        Test zipcode detection
+        """
+        for address_string, zipcode, dept_number in self.data:
+            address = self.address_parser.parse(address_string)
+            self.assertEqual(address.zipcode, zipcode)
+
+    def test_department(self):
+        """
+        Test department detection
+        """
+        for address_string, zipcode, dept_number in self.data:
+            address = self.address_parser.parse(address_string)
+            self.assertEqual(address.get_department(), dept_number)
+
 
 if __name__ == '__main__':
     unittest.main()
