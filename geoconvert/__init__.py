7f43364eb68036565106dd145069d26e047fa534
Roxane <r.bellot@octopusmind.info>
2026-02-05 17:05:14 +0100

    allow geoconvert convertion functions to be imported from geoconvert

--- geoconvert/__init__.py
+++ geoconvert/__init__.py
@@ -1,2 +1,30 @@
 __author__ = "Octopusmind <informatique@octopusmind.info>"
 __version__ = "5.3.2"
+
+
+from .convert import (
+    address_to_country_and_subdivision_codes,
+    address_to_country_code,
+    address_to_subdivision_code,
+    br_address_to_state_code,
+    br_postcode_to_state_code,
+    br_state_name_to_state_code,
+    ca_address_to_province_code,
+    ca_postcode_to_province_code,
+    ca_province_name_to_province_code,
+    capital_name_to_country_code,
+    country_name_to_country_code,
+    de_address_to_land_code,
+    de_hauptstadt_to_land_code,
+    de_land_name_to_land_code,
+    de_postcode_to_land_code,
+    fr_address_to_dept_code,
+    fr_dept_name_to_dept_code,
+    fr_postcode_to_dept_code,
+    fr_region_id_to_info,
+    fr_region_name_to_id,
+    fr_region_name_to_info,
+    us_address_to_state_code,
+    us_postcode_to_state_code,
+    us_state_name_to_state_code,
+)
